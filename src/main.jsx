ee0aef60288855bd9022bc7dea6c2e5094678a49
xeyalamanov121-commits <xeyalamanov121@gmail.com>
2026-05-21 03:22:20 +0400

    Import index.css for styling
    Added import statement for index.css to style the application.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import PokerGame from './PokerGame';
+import './index.css'; // Əgər CSS faylınız varsa, yoxdursa bu sətri silə bilərsiniz
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PokerGame />
   </React.StrictMode>
 );
+
