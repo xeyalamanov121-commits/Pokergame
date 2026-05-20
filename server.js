5763dd028dcef3e3365b84e9c5901a284d03e674
xeyalamanov121-commits <xeyalamanov121@gmail.com>
2026-05-21 01:42:37 +0400

    Update connection logs and comments in server.js

--- server.js
+++ server.js
@@ -4,26 +4,22 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server, { cors: { origin: "*" } });
 const path = require('path');
 
-// Statik faylların yeri
 app.use(express.static(path.join(__dirname, '/')));
 
 const pokerTables = { low: [], high: [] };
 
 io.on('connection', (socket) => {
-    console.log("Yeni bağlantı:", socket.id);
+    console.log("Yeni oyunçu qoşuldu:", socket.id);
 
     socket.on('joinTable', (data) => {
-        console.log("Qoşulma sorğusu gəldi:", data); // BUNU LOGS-DA GÖRMƏLİYİK
-        
         const { tableId, playerName } = data;
         if (!pokerTables[tableId]) pokerTables[tableId] = [];
         
         socket.join(tableId);
         pokerTables[tableId].push({ id: socket.id, name: playerName });
         
-        // CAVAB GÖNDƏRİRİK
+        // MÜHİM: Bu hissə bütün masadakılara siyahını göndərir
         io.to(tableId).emit('tableUpdated', pokerTables[tableId]);
-        console.log("Cavab göndərildi, otaq:", tableId);
     });
 });
 
